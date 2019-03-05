99481539f94799ebd860c316c369472cee668967
FahmiSyahrulah <fahmi@alphatech.id>
2019-03-05 14:37:00 +0700

    database

--- inventaris/barang/admin.py
+++ inventaris/barang/admin.py
@@ -1,3 +1,7 @@
+from .models import Barang
 from django.contrib import admin
 
 # Register your models here.
+my_model = [Barang]
+admin.site.register(my_model)
+
--- inventaris/barang/models.py
+++ inventaris/barang/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.utils import timezone
+from django.db.models import CharField, TextField, DateTimeField
+from django.db import models as models
 
 # Create your models here.
+class Barang(models.Model):
+    nama = models.CharField(max_length=255, default='')
+    status = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.nama
+
